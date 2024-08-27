0eb9fdbea4052ac824313e3f5ff109ead0cdb98a
Hamza Bham <92208535+MHamzaBham@users.noreply.github.com>
2024-08-27 22:07:54 +0500

    Initial files uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+(function () {
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+
+    /*=====================================
+    Sticky
+    ======================================= */
+    window.onscroll = function () {
+        var header_navbar = document.querySelector(".navbar-area");
+        var sticky = header_navbar.offsetTop;
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+        }
+        var backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+            header_navbar.classList.remove("sticky");
+        }
+    };
+
+    // WOW active
+    new WOW().init();
+
+    //===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+
+
+// Portfolio
+    document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+        let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+        let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+        let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+      
+        let initIsotope;
+        imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+          initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+            itemSelector: '.isotope-item',
+            layoutMode: layout,
+            filter: filter,
+            sortBy: sort
+          });
+        });
+      
+        isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+          filters.addEventListener('click', function() {
+            isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+            this.classList.add('filter-active');
+            initIsotope.arrange({
+              filter: this.getAttribute('data-filter')
+            });
+            if (typeof aosInit === 'function') {
+              aosInit();
+            }
+          }, false);
+        });
+      });
+})();
